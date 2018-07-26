391002749c3c5dc127651a12ee0456add261aec1
shipeilei <shipeilei@bubi.cn>
2018-07-26 09:59:07 +0800

    Updata LogCreateOperation

--- src/common/operation.go
+++ src/common/operation.go
@@ -912,7 +912,7 @@ func LogCreate(reqData model.LogCreateOperation) model.LogCreateResponse {
 		return resData
 	}
 	for i := range reqData.GetDatas() {
-		if len(reqData.GetDatas()[i]) >= 1024 && len(reqData.GetTopic()) < 0 {
+		if len(reqData.GetDatas()[i]) >= 1024 || len(reqData.GetTopic()) < 0 {
 			SDKRes := exception.GetSDKRes(exception.INVALID_LOG_DATA_ERROR)
 			resData.ErrorCode = SDKRes.ErrorCode
 			resData.ErrorDesc = SDKRes.ErrorDesc
