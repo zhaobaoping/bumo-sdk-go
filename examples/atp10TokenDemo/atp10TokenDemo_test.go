d2cb9b9a3a9d80288321b9d22b09030c47b7bdcb
shipeilei <shipeilei@bubi.cn>
2018-09-06 18:58:08 +0800

    Updata demo

---
+++ examples/atp10TokenDemo/atp10TokenDemo_test.go
@@ -0,0 +1,166 @@
+// atp10TokenDemo_test
+package atp10TokenDemo_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/bumoproject/bumo-sdk-go/src/model"
+	"github.com/bumoproject/bumo-sdk-go/src/sdk"
+)
+
+var testSdk sdk.Sdk
+
+type Atp10Metadata struct {
+	Version     string `json:"version"`
+	Name        string `json:"name"`
+	TotalSupply int64  `json:"total_supply"`
+	Decimals    int64  `json:"decimals"`
+	Description string `json:"description"`
+}
+
+//to initialize the SDK
+func Test_Init(t *testing.T) {
+	var reqData model.SDKInitRequest
+	reqData.SetUrl("http://seed1.bumotest.io:26002")
+	resData := testSdk.Init(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		t.Log("Test_NewSDK")
+	}
+}
+
+//issued assets (limited issue & unlimited issue)
+func Test_Atp10Issue(t *testing.T) {
+	var atp10Metadata Atp10Metadata
+	atp10Metadata.Version = "1.0"
+	atp10Metadata.Name = "code"
+	atp10Metadata.Decimals = 8
+	//If the TotalSupply is greater than 0, it means limited release;Less than or equal to 0 means it's an unlimited distribution
+	atp10Metadata.TotalSupply = 1000000000000
+	metadataStr, err := json.Marshal(atp10Metadata)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	var metadata string = "abc"
+	var code string = "ABC"
+	var amount int64 = 1000000
+	var issuer string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	var reqDataIssue model.AssetIssueOperation
+	reqDataIssue.Init()
+	reqDataIssue.SetAmount(amount)
+	reqDataIssue.SetCode(code)
+	reqDataIssue.SetSourceAddress(issuer)
+	reqDataIssue.SetMetadata(metadata)
+	hashIssue, ErrorDesc := atp10BlobSubmit(testSdk, reqDataIssue, string(metadataStr))
+	if ErrorDesc != "" {
+		t.Errorf(ErrorDesc)
+	} else {
+		t.Log("hash succeed", hashIssue)
+	}
+	var reqDataSend model.AssetSendOperation
+	var destAddress string = "buQtjhgK9SakQPYGzoZ3iHodfRvd8qTGoaYd"
+	var reqDataCheckActivated model.AccountCheckActivatedRequest
+	reqDataCheckActivated.SetAddress(destAddress)
+	reqDataCheckActivated := testSdk.Account.CheckActivated(reqDataCheckActivated)
+	if reqDataCheckActivated.ErrorCode != 0 || reqDataCheckActivated.Result.IsActivated == false {
+		t.Log("destAddress not Activated")
+	}
+	var sourceAddress string = "buQfnVYgXuMo3rvCEpKA6SfRrDpaz8D8A9Ea"
+	reqDataSend.SetAmount(amount)
+	reqDataSend.SetCode(code)
+	reqDataSend.SetDestAddress(destAddress)
+	reqDataSend.SetIssuer(issuer)
+	reqDataSend.SetMetadata(metadata)
+	reqDataSend.SetSourceAddress(sourceAddress)
+	hashSend, ErrorDesc := atp10BlobSubmit(testSdk, reqDataIssue, "")
+	if ErrorDesc != "" {
+		t.Errorf(ErrorDesc)
+	} else {
+		t.Log("hash succeed", hashSend)
+	}
+
+}
+
+//append the issuance of corresponding assets
+func Test_Atp10AppendToIssue(t *testing.T) {
+	code := "code"
+	issuer := "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	var metadata string = "abc"
+	var amount int64 = 1000000
+	var reqDataIssue model.AssetIssueOperation
+	reqDataIssue.Init()
+	reqDataIssue.SetAmount(amount)
+	reqDataIssue.SetCode(code)
+	reqDataIssue.SetSourceAddress(issuer)
+	reqDataIssue.SetMetadata(metadata)
+	hashIssue, ErrorDesc := atp10BlobSubmit(testSdk, reqDataIssue, "")
+	if ErrorDesc != "" {
+		t.Errorf(ErrorDesc)
+	} else {
+		t.Log("hash succeed", hashIssue)
+	}
+	var reqDataSend model.AssetSendOperation
+	var destAddress string = "buQtjhgK9SakQPYGzoZ3iHodfRvd8qTGoaYd"
+	var reqDataCheckActivated model.AccountCheckActivatedRequest
+	reqDataCheckActivated.SetAddress(destAddress)
+	reqDataCheckActivated := testSdk.Account.CheckActivated(reqDataCheckActivated)
+	if reqDataCheckActivated.ErrorCode != 0 || reqDataCheckActivated.Result.IsActivated == false {
+		t.Log("destAddress not Activated")
+	}
+	var sourceAddress string = "buQfnVYgXuMo3rvCEpKA6SfRrDpaz8D8A9Ea"
+	reqDataSend.SetAmount(amount)
+	reqDataSend.SetCode(code)
+	reqDataSend.SetDestAddress(destAddress)
+	reqDataSend.SetIssuer(issuer)
+	reqDataSend.SetMetadata(metadata)
+	reqDataSend.SetSourceAddress(sourceAddress)
+	hashSend, ErrorDesc := atp10BlobSubmit(testSdk, reqDataIssue, "")
+	if ErrorDesc != "" {
+		t.Errorf(ErrorDesc)
+	} else {
+		t.Log("hash succeed", hashSend)
+	}
+
+}
+
+func atp10BlobSubmit(testSdk sdk.Sdk, reqDataOperation model.BaseOperation, metadata string) (hash string, ErrorDesc string) {
+	//Blob
+	var reqDataBlob model.TransactionBuildBlobRequest
+	var sourceAddressBlob string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	reqDataBlob.SetSourceAddress(sourceAddressBlob)
+	var feeLimit int64 = 1000000000
+	reqDataBlob.SetFeeLimit(feeLimit)
+	var gasPrice int64 = 1000
+	reqDataBlob.SetGasPrice(gasPrice)
+	var nonce int64 = 97
+	reqDataBlob.SetNonce(nonce)
+	reqDataBlob.SetMetadata(metadata)
+	reqDataBlob.SetOperation(reqDataOperation)
+	resDataBlob := testSdk.Transaction.BuildBlob(reqDataBlob)
+	if resDataBlob.ErrorCode != 0 {
+		return "", resDataBlob.ErrorDesc
+	} else {
+		//Sign
+		PrivateKey := []string{"PrivateKey"}
+		var reqData model.TransactionSignRequest
+		reqData.SetBlob(resDataBlob.Result.Blob)
+		reqData.SetPrivateKeys(PrivateKey)
+		resDataSign := testSdk.Transaction.Sign(reqData)
+		if resDataSign.ErrorCode != 0 {
+			return "", resDataSign.ErrorDesc
+		} else {
+			//Submit
+			var reqData model.TransactionSubmitRequest
+			reqData.SetBlob(resDataBlob.Result.Blob)
+			reqData.SetSignatures(resDataSign.Result.Signatures)
+			resDataSubmit := testSdk.Transaction.Submit(reqData)
+			if resDataSubmit.ErrorCode != 0 {
+				return "", resDataSubmit.ErrorDesc
+			} else {
+				return resDataBlob.Result.Blob, resDataBlob.ErrorDesc
+			}
+		}
+	}
+}
---
+++ examples/digitalAssetsDemo/digitalAssetsDemo_test.go
@@ -0,0 +1,372 @@
+// digitalAssetsDemo
+package digitalAssetsDemo_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/bumoproject/bumo-sdk-go/src/model"
+	"github.com/bumoproject/bumo-sdk-go/src/sdk"
+)
+
+var testSdk sdk.Sdk
+
+//to initialize the sdk
+func Test_Init(t *testing.T) {
+	var reqData model.SDKInitRequest
+	reqData.SetUrl("http://seed1.bumotest.io:26002")
+	resData := testSdk.Init(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		t.Log("Test_NewSDK")
+	}
+}
+
+//check that the blocks are synchronized
+func Test_Block_CheckStatus(t *testing.T) {
+	resData := testSdk.Block.CheckStatus()
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		t.Log("IsSynchronous:", resData.Result.IsSynchronous)
+		t.Log("Test_Block_CheckStatus succeed", resData.Result)
+	}
+
+}
+
+//create account
+func Test_Account_Create(t *testing.T) {
+	resData := testSdk.Account.Create()
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		t.Log("Test_Account_Create", resData.Result)
+	}
+}
+
+//verify account address
+func Test_Account_checkValid(t *testing.T) {
+	var reqData model.AccountCheckValidRequest
+	var address string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	reqData.SetAddress(address)
+	resData := testSdk.Account.CheckValid(reqData)
+
+	if resData.Result.IsValid {
+		t.Log("Test_Account_CheckAddress succeed", resData.Result.IsValid)
+	} else {
+		t.Error("Test_Account_CheckAddress failured")
+	}
+}
+
+//enquiry of account details
+func Test_Account_GetInfo(t *testing.T) {
+	var reqData model.AccountGetInfoRequest
+	var address string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	reqData.SetAddress(address)
+	resData := testSdk.Account.GetInfo(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		data, _ := json.Marshal(resData.Result)
+		t.Log("info:", string(data))
+		t.Log("Test_Account_GetInfo succeed", resData.Result)
+	}
+}
+
+//checking account balance
+func Test_Account_GetBalance(t *testing.T) {
+	var reqData model.AccountGetBalanceRequest
+	var address string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	reqData.SetAddress(address)
+	resData := testSdk.Account.GetBalance(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		t.Log("Balance:", resData.Result.Balance)
+		t.Log("Test_Account_GetBalance succeed", resData.Result)
+	}
+}
+
+//get account assets
+func Test_Account_GetAssets(t *testing.T) {
+	var reqData model.AccountGetAssetsRequest
+	var address string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	reqData.SetAddress(address)
+	resData := testSdk.Account.GetAssets(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		data, _ := json.Marshal(resData.Result.Assets)
+		t.Log("Assets:", string(data))
+		t.Log("Test_Account_GetAssets succeed", resData.Result)
+
+	}
+}
+
+//get account metadata
+func Test_Account_GetMetadata(t *testing.T) {
+	var reqData model.AccountGetMetadataRequest
+	var address string = "buQXoNR24p2pPqnXPyiDprmTWsU4SYLtBNCG"
+	reqData.SetAddress(address)
+	reqData.SetKey("global_attribute")
+	resData := testSdk.Account.GetMetadata(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		data, _ := json.Marshal(resData.Result.Metadatas[0].Value)
+
+		t.Log("Metadatas:", string(data))
+		t.Log("Test_Account_GetMetadata succeed", resData.Result)
+	}
+}
+
+//get asset info
+func Test_Asset_GetInfo(t *testing.T) {
+	var reqData model.AssetGetInfoRequest
+	var address string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	reqData.SetAddress(address)
+	reqData.SetIssuer("buQnc3AGCo6ycWJCce516MDbPHKjK7ywwkuo")
+	reqData.SetCode("HNC")
+	resData := testSdk.Token.Asset.GetInfo(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		data, _ := json.Marshal(resData.Result.Assets)
+		t.Log("Assets:", string(data))
+		t.Log("Test_Asset_GetInfo succeed", resData.Result.Assets)
+	}
+}
+
+//get contract info
+func Test_Contract_GetInfo(t *testing.T) {
+	var reqData model.ContractGetInfoRequest
+	var address string = "buQXmYrmqt6ohcKtLFKgWFSZ5CjYKaSzaMjT"
+	reqData.SetAddress(address)
+	resData := testSdk.Contract.GetInfo(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		data, _ := json.Marshal(resData.Result.Contract)
+		t.Log("Contract:", string(data))
+		t.Log("Test_Contract_GetInfo succeed", resData.Result)
+	}
+}
+
+//check the account transaction serial number
+func Test_Account_GetNonce(t *testing.T) {
+	var reqData model.AccountGetNonceRequest
+	var address string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	reqData.SetAddress(address)
+	resData := testSdk.Account.GetNonce(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		t.Log("Nonce:", resData.Result.Nonce)
+		t.Log("Test_Account_GetNonce succeed", resData.Result)
+	}
+}
+
+//enquiry of transaction details
+func Test_Transaction_GetInfo(t *testing.T) {
+	var reqData model.TransactionGetInfoRequest
+	var hash string = "c738fb80dc401d6aba2cf3802ec85ac07fbc23366c003537b64cd1a59ab307d8"
+	reqData.SetHash(hash)
+	resData := testSdk.Transaction.GetInfo(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		data, _ := json.Marshal(resData.Result)
+		t.Log("info:", string(data))
+		t.Log("Test_Transaction_GetInfo succeed", resData.Result)
+	}
+}
+
+//get block height
+func Test_Block_GetNumber(t *testing.T) {
+	resData := testSdk.Block.GetNumber()
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		t.Log("BlockNumber:", resData.Result.Header.BlockNumber)
+		t.Log("Test_Block_GetNumber", resData.Result)
+	}
+}
+
+//get block details
+func Test_Block_GetInfo(t *testing.T) {
+	var reqData model.BlockGetInfoRequest
+	var blockNumber int64 = 581283
+	reqData.SetBlockNumber(blockNumber)
+	resData := testSdk.Block.GetInfo(reqData)
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		data, _ := json.Marshal(resData.Result.Header)
+		t.Log("Header:", string(data))
+		t.Log("Test_Block_GetInfo succeed", resData.Result)
+	}
+}
+
+//get the latest block information
+func Test_Block_GetLatest(t *testing.T) {
+	resData := testSdk.Block.GetLatest()
+	if resData.ErrorCode != 0 {
+		t.Errorf(resData.ErrorDesc)
+	} else {
+		data, _ := json.Marshal(resData.Result.Header)
+		t.Log("Header:", string(data))
+		t.Log("Test_Block_GetLatest succeed", resData.Result)
+	}
+}
+
+//evaluate fee
+func Test_Transaction_EvaluateFee(t *testing.T) {
+	var reqDataOperation model.BUSendOperation
+	reqDataOperation.Init()
+	var amount int64 = 100
+	reqDataOperation.SetAmount(amount)
+	reqDataOperation.SetMetadata("63")
+	var destAddress string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	reqDataOperation.SetDestAddress(destAddress)
+
+	var reqDataEvaluate model.TransactionEvaluateFeeRequest
+	var sourceAddress string = "buQVU86Jm4FeRW4JcQTD9Rx9NkUkHikYGp6z"
+	reqDataEvaluate.SetSourceAddress(sourceAddress)
+	var nonce int64 = 5
+	reqDataEvaluate.SetNonce(nonce)
+	var signatureNumber string = "3"
+	reqDataEvaluate.SetSignatureNumber(signatureNumber)
+	var SetCeilLedgerSeq int64 = 50
+	reqDataEvaluate.SetCeilLedgerSeq(SetCeilLedgerSeq)
+	reqDataEvaluate.SetMetadata("63")
+	reqDataEvaluate.SetOperation(reqDataOperation)
+	resDataEvaluate := testSdk.Transaction.EvaluateFee(reqDataEvaluate)
+	if resDataEvaluate.ErrorCode != 0 {
+		t.Log(resDataEvaluate)
+		t.Errorf(resDataEvaluate.ErrorDesc)
+	} else {
+		data, _ := json.Marshal(resDataEvaluate.Result)
+		t.Log("Evaluate:", string(data))
+		t.Log("Test_EvaluateFee succeed", resDataEvaluate.Result)
+	}
+}
+
+//Activate Account
+func Test_activate_Account(t *testing.T) {
+	//Operation
+	var reqDataOperation model.AccountActivateOperation
+	reqDataOperation.Init()
+	var initBalance int64 = 1000000000
+	var nonce int64 = 98
+	var destAddress string = "buQVU86Jm4FeRW4JcQTD9Rx9NkUkHikYGp6z"
+	reqDataOperation.SetDestAddress(destAddress)
+	reqDataOperation.SetMetadata("send")
+	reqDataOperation.SetInitBalance(initBalance)
+
+	errorCode, errorDesc, hash := Transaction_BuildBlob_Sign_Submit(reqDataOperation, nonce)
+	if errorCode != 0 {
+		t.Log("errorDesc:", errorDesc)
+	} else {
+		t.Log("Test_BU_Send succeed", hash)
+	}
+
+}
+
+//Asset Issue
+func Test_Asset_Issue(t *testing.T) {
+	//Operation
+	var reqDataOperation model.AssetIssueOperation
+	reqDataOperation.Init()
+	var amount int64 = 100
+	var nonce int64 = 98
+	//	var destAddress string = "buQVU86Jm4FeRW4JcQTD9Rx9NkUkHikYGp6z"
+	reqDataOperation.SetAmount(amount)
+	reqDataOperation.SetMetadata("")
+	reqDataOperation.SetCode("ABC")
+	errorCode, errorDesc, hash := Transaction_BuildBlob_Sign_Submit(reqDataOperation, nonce)
+	if errorCode != 0 {
+		t.Log("errorDesc:", errorDesc)
+
+	} else {
+		t.Log("Test_BU_Send succeed", hash)
+	}
+}
+
+//Asset Send
+func Test_Asset_Send(t *testing.T) {
+	//Operation
+	var reqDataOperation model.AssetSendOperation
+	reqDataOperation.Init()
+	var amount int64 = 100
+	var nonce int64 = 98
+	var destAddress string = "buQVU86Jm4FeRW4JcQTD9Rx9NkUkHikYGp6z"
+	var issuer string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	reqDataOperation.SetAmount(amount)
+	reqDataOperation.SetMetadata("send")
+	reqDataOperation.SetCode("ABC")
+	reqDataOperation.SetDestAddress(destAddress)
+	reqDataOperation.SetIssuer(issuer)
+
+	errorCode, errorDesc, hash := Transaction_BuildBlob_Sign_Submit(reqDataOperation, nonce)
+	if errorCode != 0 {
+		t.Log("errorDesc:", errorDesc)
+	} else {
+		t.Log("Test_BU_Send succeed", hash)
+	}
+}
+
+//BU Send
+func Test_BU_Send(t *testing.T) {
+	//Operation
+	var reqDataOperation model.BUSendOperation
+	reqDataOperation.Init()
+	var amount int64 = 100
+	var nonce int64 = 111
+	var destAddress string = "buQVU86Jm4FeRW4JcQTD9Rx9NkUkHikYGp6z"
+	reqDataOperation.SetAmount(amount)
+	reqDataOperation.SetMetadata("send")
+	reqDataOperation.SetDestAddress(destAddress)
+	errorCode, errorDesc, hash := Transaction_BuildBlob_Sign_Submit(reqDataOperation, nonce)
+	if errorCode != 0 {
+		t.Log("errorDesc:", errorDesc)
+	} else {
+		t.Log("Test_BU_Send succeed", hash)
+	}
+}
+func Transaction_BuildBlob_Sign_Submit(reqDataOperation model.BaseOperation, nonce int64) (errorCode int, errorDesc string, hash string) {
+	//Blob
+	var reqDataBlob model.TransactionBuildBlobRequest
+	var sourceAddressBlob string = "buQemmMwmRQY1JkcU7w3nhruoX5N3j6C29uo"
+	reqDataBlob.SetSourceAddress(sourceAddressBlob)
+	var feeLimit int64 = 1000000000
+	reqDataBlob.SetFeeLimit(feeLimit)
+	var gasPrice int64 = 1000
+	reqDataBlob.SetGasPrice(gasPrice)
+	reqDataBlob.SetNonce(nonce)
+	reqDataBlob.SetMetadata("63")
+	reqDataBlob.SetOperation(reqDataOperation)
+
+	resDataBlob := testSdk.Transaction.BuildBlob(reqDataBlob)
+	if resDataBlob.ErrorCode != 0 {
+		return resDataBlob.ErrorCode, resDataBlob.ErrorDesc, ""
+	} else {
+		//Sign
+		PrivateKey := []string{"privKey"}
+		var reqData model.TransactionSignRequest
+		reqData.SetBlob(resDataBlob.Result.Blob)
+		reqData.SetPrivateKeys(PrivateKey)
+
+		resDataSign := testSdk.Transaction.Sign(reqData)
+		if resDataSign.ErrorCode != 0 {
+			return resDataSign.ErrorCode, resDataSign.ErrorDesc, ""
+		} else {
+			//Submit
+			var reqData model.TransactionSubmitRequest
+			reqData.SetBlob(resDataBlob.Result.Blob)
+			reqData.SetSignatures(resDataSign.Result.Signatures)
+			resDataSubmit := testSdk.Transaction.Submit(reqData)
+			return resDataSubmit.ErrorCode, resDataSubmit.ErrorDesc, resDataSubmit.Result.Hash
+		}
+	}
+}
