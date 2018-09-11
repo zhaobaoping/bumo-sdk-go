4f1293ede80c2fad47a9c41545ba35781b1a00a5
shipeilei <shipeilei@bubi.cn>
2018-09-11 10:03:21 +0800

    Updata demo

--- examples/offlineSignatureDemo/offlineSignatureDemo_test.go
+++ examples/offlineSignatureDemo/offlineSignatureDemo_test.go
@@ -3,8 +3,6 @@ package offlineSignatureDemo_test
 
 import (
 	"encoding/hex"
-
-	"fmt"
 	"testing"
 
 	"github.com/bumoproject/bumo-sdk-go/src/crypto/keypair"
@@ -32,8 +30,8 @@ func Test_Init(t *testing.T) {
 //submit transaction
 func Test_submitTransaction(t *testing.T) {
 	var reqData model.TransactionSubmitRequest
-	var transactionBlob string = "0a24627551656d6d4d776d525159314a6b63553777336e6872756f58354e336a36433239756f106d18c0843d20e80728eff135320236333a3008071a02363352280a24627551565538364a6d3446655257344a63515444395278394e6b556b48696b594770367a1064"
-	var signData string = "5aac965b327c71555244d1589344a4000c9673380907d1c9bc9ab53dc4e7a39c6f5f5b9f8daae702f3e8ffdf10cdc2ef8d7aa30d3894ab54ff2de9deb03a8305"
+	var transactionBlob string = "0a246275516e6e5545425245773268423670574847507a77616e5837643238786b364b566370106f1880c2d72f20e8073a57080712246275516e6e5545425245773268423670574847507a77616e5837643238786b364b566370522d0a24627551426a4a443142534a376e7a41627a6454656e416870466a6d7852564545746d78481080d0dbc3f402"
+	var signData string = "b22393ec70b35c3042157311a6ed30d6d5e94ea330ce78842db875018650fb84302854c751f8c969dd3c9d01ab649162f43194bd1fa917e13917dd10b2a22e0e"
 	var publicKey string = "b001ebb9f88123658f0a62c49fb5cfbc265cc56ee144a56452012ef2abff7f9ef7974992926b"
 	signatures := []model.Signature{
 		{
@@ -63,49 +61,50 @@ func Test_Online_BuildTransactionBlob(t *testing.T) {
 	// Set up the maximum cost 0.01BU
 	var feeLimit int64 = 100000000
 	// Transaction initiation account's nonce + 1
-	var nonce int64 = 0
+	var nonce int64 = 111
 	errorCode, errorDesc, transactionBlobResult := buildTransactionBlob(senderAddresss, nonce, destAddress, amount, feeLimit, gasPrice)
 	if errorCode != 0 {
-		fmt.Println(errorDesc)
+		t.Errorf(errorDesc)
 	} else {
-		fmt.Println(transactionBlobResult)
+		t.Log("Blob:", transactionBlobResult)
 	}
 
 }
 
-//func Test_Offline_ParseBlob(t *testing.T) {
-//	// Get transactionBlobResult from 1 (Network Environment)
-//	var blob string = "0a24627551656d6d4d776d525159314a6b63553777336e6872756f58354e336a36433239756f106d18c0843d20e80728eff135320236333a3008071a02363352280a24627551565538364a6d3446655257344a63515444395278394e6b556b48696b594770367a1064"
-//	// Parsing the transaction Blob
-//	transaction := ParseBlob(blob)
-//	if transaction != true {
-//		fmt.Println(blob)
-//	} else {
-//		fmt.Println("blob is false")
-//	}
-//}
-//func Test_Offline_SignTransactionBlob(t *testing.T) {
-//	// When the transaction Blob is confirmed, it begins to sign a signature
-
-//	// Transaction Blob
-//	var transactionBlob string = "0a24627551656d6d4d776d525159314a6b63553777336e6872756f58354e336a36433239756f106d18c0843d20e80728eff135320236333a3008071a02363352280a24627551565538364a6d3446655257344a63515444395278394e6b556b48696b594770367a1064"
-//	// The account private key to send BU
-//	var senderPrivateKey string = "privbyQCRp7DLqKtRFCqKQJr81TurTqG6UKXMMtGAmPG3abcM9XHjWvq"
-
-//	// Sign transaction
-//	signature := signTransaction(transactionBlob, senderPrivateKey)
-//	fmt.Println(signature)
-//}
-func signTransaction(transactionBlob string, senderPrivateKey string) string {
-	privateKey, err := keypair.GetEncPublicKey(senderPrivateKey)
-	if err != nil {
-		return ""
+func Test_Offline_ParseBlob(t *testing.T) {
+	// Get transactionBlobResult from 1 (Network Environment)
+	var blob string = "0a24627551656d6d4d776d525159314a6b63553777336e6872756f58354e336a36433239756f106d18c0843d20e80728eff135320236333a3008071a02363352280a24627551565538364a6d3446655257344a63515444395278394e6b556b48696b594770367a1064"
+	// Parsing the transaction Blob
+	transaction := ParseBlob(blob)
+	if transaction == true {
+		t.Log("Blob:", blob)
+	} else {
+		t.Errorf("blob is false")
 	}
-	SignData, err := signature.Sign(privateKey, []byte(transactionBlob))
+}
+func Test_Offline_SignTransactionBlob(t *testing.T) {
+	// When the transaction Blob is confirmed, it begins to sign a signature
+
+	// Transaction Blob
+	var transactionBlob string = "6bde11ac70bcfb3fbe2e1fa9d7d73c5c932db9ae84674bec86f1905aeea7c974ef81a9a9281f20f8bbbfd86b4f3a40510930d3654832f0c6e19cd0ead4cff205"
+	// The account private key to send BU
+	var senderPrivateKey string = "privbtYzJ6miiFktK9BsDAMRNd3J4eKkuszfXqJ2huQ2h8DGUnRs9nuq"
+
+	// Sign transaction
+	signature, publicKey := signTransaction(transactionBlob, senderPrivateKey)
+	t.Log("signature:", signature)
+	t.Log("publicKey:", publicKey)
+}
+func signTransaction(transactionBlob string, senderPrivateKey string) (signData string, publicKey string) {
+	publicKey, err := keypair.GetEncPublicKey(senderPrivateKey)
 	if err != nil {
-		return ""
+		return "", ""
 	}
-	return SignData
+	signData, err = signature.Sign(senderPrivateKey, []byte(transactionBlob))
+	if err != nil {
+		return "", ""
+	}
+	return signData, publicKey
 }
 
 func ParseBlob(blob string) bool {
@@ -127,6 +126,7 @@ func ParseBlob(blob string) bool {
 func buildTransactionBlob(senderAddress string, nonce int64, destAddress string, amount int64, feeLimit int64, gasPrice int64) (errorCode int, errorDesc string, blob string) {
 	// Build sendBU
 	var reqDataOperation model.BUSendOperation
+	reqDataOperation.Init()
 	reqDataOperation.SetAmount(amount)
 	reqDataOperation.SetDestAddress(destAddress)
 	reqDataOperation.SetSourceAddress(senderAddress)
